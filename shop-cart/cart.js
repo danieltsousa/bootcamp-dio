5a4b7c80c039868a6df4042a86256093a5b8ab5f
danieltsousa <167568247+danieltsousa@users.noreply.github.com>
2026-04-28 12:19:49 -0300

    Update cart.js

--- shop-cart/cart.js
+++ shop-cart/cart.js
@@ -10,6 +10,12 @@ async function deleteItem(userCart, name) {
 async function removeItem(userCart, index) {
     
 }
+ async function displayCart(userCart, index) {
+     console.log("Shopee cart list:")
+     userCart.forEach((item, index) => {
+         console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantify} | Subtotal ${item.subtotal()}`);
+ });
+ }
 
 async function calculateTotal(userCart) {
      const result =  userCart.reduce((total, item) => total + item.subtotal());
